16eb399643ae75e9a06de5b502a1164b1fd50c06
ademcan3460 <43909717+ademcan3460@users.noreply.github.com>
2018-10-06 20:56:02 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -79,13 +79,9 @@ client.unload = command => {
 };
 
 client.on('message', msg => {
-  if (msg.content.toLowerCase() === 'sa') {
-		if (!msg.guild.member(msg.author).hasPermission("BAN_MEMBERS")) {
-			msg.author.sendMessage('Aleyküm selam,  hoş geldin ^^'); 
-		} else {
-		msg.reply('Aleyküm selam, hoş geldin ^^');
-		}
-	}
+  if (msg.content === 'sa') {
+    msg.reply('**Aleyküm Selam HoşGeldin :)**');
+  }
 });
 
 ////////////////////////
