c1845eae8d82164391e9484d13def94da683edfc
ademcan3460 <43909717+ademcan3460@users.noreply.github.com>
2018-10-07 21:16:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,6 @@ const client = new Discord.Client();
 const ayarlar = require('./ayarlar.json');
 const chalk = require('chalk');
 const fs = require('fs');
-const moment = require('moment');
 require('./util/eventLoader')(client);
 
 var prefix = ayarlar.prefix;
